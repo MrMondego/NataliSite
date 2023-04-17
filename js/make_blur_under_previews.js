89f06b4d9d827f9054c0ef1363f8be36465bd49f
MrMondego <mondegosol1@mail.ru>
2023-04-18 01:50:24 +0300

    auto blur depending on image

---
+++ js/make_blur_under_previews.js
@@ -0,0 +1,12 @@
+addEventListener("load", () => {
+  
+  const galleryElements = document.getElementsByClassName("gallery-category");
+  for(let i = 0; i < galleryElements.length; i++) {
+    let imageName = getImageDirectoryByFullURL(galleryElements[i].children[1].attributes.getNamedItem("src").value)
+    // console.log(imageName.indexOf(" ") != -1 ? imageName.replace(" ", "\ ") : imageName)
+    galleryElements[i].style.backgroundImage = `url('../../image/previews/${imageName}')`;
+  }
+})
+function getImageDirectoryByFullURL(url){
+    return url.split('/').pop()
+}
