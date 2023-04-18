f0438d5bc8f127e7cb861a906cc3112ac47e03ef
MrMondego <mondegosol1@mail.ru>
2023-04-18 13:41:56 +0300

    js blur fix

--- js/make_blur_under_previews.js
+++ js/make_blur_under_previews.js
@@ -4,7 +4,7 @@ addEventListener("load", () => {
   for(let i = 0; i < galleryElements.length; i++) {
     let imageName = getImageDirectoryByFullURL(galleryElements[i].children[1].attributes.getNamedItem("src").value)
     // console.log(imageName.indexOf(" ") != -1 ? imageName.replace(" ", "\ ") : imageName)
-    galleryElements[i].style.backgroundImage = `url('../../image/previews/${imageName}')`;
+    galleryElements[i].style.backgroundImage = `url('image/previews/${imageName}')`;
   }
 })
 function getImageDirectoryByFullURL(url){
