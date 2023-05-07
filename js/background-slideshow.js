710376a5bb27a8b221a71025a876f0c07203e919
MrMondego <mondegosol1@mail.ru>
2023-05-07 03:06:12 +0300

    finally some good background

--- js/background-slideshow.js
+++ js/background-slideshow.js
@@ -1,13 +1,27 @@
 // var images= [ '../image/top_background-1.jpg',
 //             '../image/top_background-2.jpg',
 //             '../image/top_background-3.jpg' ];
-const backgrounds = ["bg-1", "bg-2", "bg-3"];
+// const backgrounds = ["bg-1", "bg-2", "bg-3"];
 let currentBg = 0;
-
+const backgrounds = $(".bgWrapper").children()
+const backgroundsCount = backgrounds.length;
+// function setBackground() {
+//    $(".bgWrapper__bg").removeClass(backgrounds[currentBg]);
+//    currentBg = (currentBg + 1) % backgrounds.length;
+//    $(".bgWrapper__bg").addClass(backgrounds[currentBg]);
+// }
 function setBackground() {
-   $(".bgWrapper__bg").removeClass(backgrounds[currentBg]);
-   currentBg = (currentBg + 1) % backgrounds.length;
-   $(".bgWrapper__bg").addClass(backgrounds[currentBg]);
+   // backgrounds.eq(currentBg).css("display","block");
+   let nextBg = (currentBg + 1) % backgrounds.length;
+   // backgrounds.eq(nextBg).css("display","block");
+   backgrounds.eq(currentBg).toggleClass("active");
+   backgrounds.eq(nextBg).toggleClass("active");
+   currentBg = nextBg;
+   // backgrounds.eq(currentBg).on('transitionend webkitTransitionEnd oTransitionEnd', function() {
+   //    backgrounds.eq(currentBg).css("display","none");
+   //    currentBg = nextBg;
+   // })
 }
 
-setInterval(setBackground, 5000);
+
+setInterval(setBackground, 7000);
